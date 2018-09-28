383a1df92a238ea445b9609972ba8ba0ca6b9d87
Mike Wheaton <mike@mikewheaton.ca>
2018-09-28 08:46:45 -0700

    Update home content

--- src/components/home.js
+++ src/components/home.js
@@ -13,32 +13,36 @@ export default () => {
       <div class="words">
         <p>hi, i&rsquo;m mike wheaton.</p>
         <p>
-          user experience engineer at microsoft, building the{' '}
+          user experience engineer at microsoft, building onedrive, sharepoint,
+          and the{' '}
           <a href="https://developer.microsoft.com/en-us/fabric">fabric</a>{' '}
-          design system and products for collaboration and cloud storage with
-          some terrific people.
+          design system that's used for apps across the company and beyond.
         </p>
         <p>
           originally from canada, now residing in beautiful seattle. it
           doesn&rsquo;t rain as much as you think it does. but almost.
         </p>
         <p>
-          <span title="as of September 10, 2018">currently</span>: getting back
+          <span title="as of September 28, 2018">currently</span>: getting back
           into running. kicking off a{' '}
-          <a href="http://www.lurn.live/">side project</a> to gain familiarity
-          with full-stack development. exploring, with recent trips to austin
-          and denver. learning to draw and paint.
+          <a href="http://www.lurn.today/">side project</a> to learn full-stack
+          development. rebuilding this site on{' '}
+          <a href="https://www.gatsbyjs.org/">gatsby</a>. exploring, with recent
+          trips to austin, denver, and portland.
         </p>
         <p>find me on:</p>
         <ul>
-          <li>
-            <a href="mailto:mike@mikewheaton.ca">email</a>
-          </li>
           <li>
             <a href="https://github.com/mikewheaton">github</a>
           </li>
           <li>
             <a href="https://www.linkedin.com/in/wheatonm">linkedin</a>
+          </li>
+          <li>
+            <a href="https://twitter.com/themikewheaton">twitter</a>
+          </li>
+          <li>
+            <a href="mailto:mike@wheaton.design">mail</a>
           </li>
         </ul>
       </div>
