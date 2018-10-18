e652fe49bf3a6f88fa01760842b63f1d8ad2b251
Mike Wheaton <mike@mikewheaton.ca>
2018-10-17 20:55:42 -0700

    Convert Note

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,7 +1,28 @@
 import { graphql } from 'gatsby';
 import React from 'react';
-import './Note.css';
+import styled from 'styled-components';
+
 import Page from '../Page/Page';
+
+const Root = styled.div`
+  p {
+    font-family: 'Overpass', sans-serif;
+    font-size: 1.25rem;
+  }
+
+  pre {
+    overflow-x: scroll;
+  }
+`;
+
+const Date = styled.div`
+  color: var(--secondary);
+  font-family: var(--monospace);
+`;
+
+const Title = styled.h1`
+  font-size: 44px;
+`;
 
 export default class Note extends React.Component {
   render() {
@@ -10,16 +31,17 @@ export default class Note extends React.Component {
 
     return (
       <Page>
-        <div className="Note">
-          <div className="Note-date">{frontmatter.date}</div>
-          <h1 className="Note-title">{frontmatter.title}</h1>
+        <Root>
+          <Date>{frontmatter.date}</Date>
+          <Title>{frontmatter.title}</Title>
           <div dangerouslySetInnerHTML={{ __html: html }} />
-        </div>
+        </Root>
       </Page>
     );
   }
 }
 
+/* @todo: By what magic does Gatsby connect this query to the component?! */
 export const pageQuery = graphql`
   query($path: String!) {
     markdownRemark(frontmatter: { path: { eq: $path } }) {
