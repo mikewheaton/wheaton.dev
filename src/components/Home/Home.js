2607c14ad4625142b4a27309b1b3691c4c67fe28
Mike Wheaton <mike@mikewheaton.ca>
2018-12-16 12:40:29 -0800

    Update the currently paragraph

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -78,12 +78,12 @@ const Home = () => {
           doesn&rsquo;t rain as much as you think it does. but almost.
         </p>
         <p>
-          <span title="as of September 28, 2018">currently</span>: getting back
-          into running. kicking off a fun{' '}
-          <a href="http://www.lurn.today/">side project</a>. exploring, with
-          recent trips to austin, denver, and portland. rebuilding this site on{' '}
-          <a href="https://www.gatsbyjs.org/">gatsby</a> (love it) with a page
-          to share <Link to="notes">notes on what i'm learning</Link>.
+          <span title="as of December 16, 2018">currently</span>: fighting jet
+          lag after a month of traveling and working remotely in the
+          philippines. getting back into running and spin classes. kicking off a
+          fun <a href="http://www.lurn.today/">side project</a>. rebuilding this
+          site on <a href="https://www.gatsbyjs.org/">gatsby</a> (love it) with
+          a page to share <Link to="notes">notes on what i'm learning</Link>.
         </p>
         <p>find me on:</p>
         <LinkList>
